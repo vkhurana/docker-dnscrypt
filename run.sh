34001255a38d0cd9c941321e6763cc3c5c265a91
RiX <rix1337@users.noreply.github.com>
2017-12-28 12:00:07 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,11 @@ RESOLVER_1=${RESOLVER_NAME:-"dnscrypt.eu-nl"}
 RESOLVER_2=${RESOLVER_NAME:-"dns.btr.zone"}
 RESOLVER_3=${RESOLVER_NAME:-"securedns"}
 
+mkdir -p /opt/dnscrypt-proxy/share/dnscrypt-proxy/
+cd /opt/dnscrypt-proxy/share/dnscrypt-proxy/
+wget -O "dnscrypt-resolvers.csv" https://raw.githubusercontent.com/jedisct1/dnscrypt-resolvers/master/v1/dnscrypt-resolvers.csv
+ 
+
 /opt/dnscrypt-proxy/sbin/dnscrypt-proxy --local-address=0.0.0.0:53 --resolver-name=${RESOLVER_1} &&
 /opt/dnscrypt-proxy/sbin/dnscrypt-proxy --local-address=0.0.0.0:53 --resolver-name=${RESOLVER_2} &&
 /opt/dnscrypt-proxy/sbin/dnscrypt-proxy --local-address=0.0.0.0:53 --resolver-name=${RESOLVER_3}
